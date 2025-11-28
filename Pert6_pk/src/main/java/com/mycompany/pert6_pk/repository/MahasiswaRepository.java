e24cbeb7aa241647bb04eb71dba3ffca580c677a
Pajar kurniawan <pajar@gmail.com>
2025-11-28 14:40:42 +0700

    first commit

---
+++ Pert6_pk/src/main/java/com/mycompany/pert6_pk/repository/MahasiswaRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert6_pk.repository;
+
+/**
+ *
+ * @author Jelly
+ */
+import com.mycompany.pert6_pk.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Integer> {
+
+
+}
---
+++ spring_pert5/spring_pert5/src/main/java/com/mycompany/spring_pert5/repository/MahasiswaRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.spring_pert5.repository;
+
+/**
+ *
+ * @author reffi
+ */
+import com.mycompany.spring_pert5.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Long> {
+
+}
+
